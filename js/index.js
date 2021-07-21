8cee427b42a74e9098fc8a8abc1c109aaf51b5da
MissingT <79103459+MissingT@users.noreply.github.com>
2021-07-21 13:33:07 -0400

    Working Git File, de pinga

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$(".logo").on("click", function(){
+    if ($(window).width() < 601-17){
+      $(".navButtons").slideToggle();
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+	$("#desktopButt").click(function(){
+    	$(".nsaMobile").hide()
+    $(".nsaLaptop").show();
+    });
+  $("#mobileButt").click(function(){
+    $(".nsaMobile").show().css("display", "block")
+    $(".nsaLaptop").hide();
+  });
+});
